4cc07e8baf6f05e130ddfa4a48e74401746fa894
YourDarkestFear <someoneteachs@gmail.com>
2025-10-19 16:20:18 +0300

    upload

---
+++ public/js/main.js
@@ -0,0 +1,2 @@
+// Simple JS placeholder - you can add interactivity here
+console.log('Hacı Ayşe Ateş Anadolu Lisesi sitesi yüklendi');
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(expressLayouts);
+app.set("layout", "layout");
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/hakkimizda", (req, res) => {
+  res.render("about");
+});
+
+app.get("/projeler", (req, res) => {
+  res.render("projects");
+});
+
+app.get("/iletisim", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/kulup", (req, res) => {
+  res.render("club");
+});
+
+app.listen(PORT, () => console.log(`🌐 Sunucu çalışıyor: http://localhost:${PORT}`));
